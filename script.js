7939584562f4f4386762f2993a0049f5f1bc9a9a
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-09-03 17:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const student = {
+     name: xyz
+}
+
+Array.prototype.getKeys = function (fn){
+      const result = [];
+
+      for(let i=0; i<getKeys.length; i++){
+          result.push(fn(getKeys[i], i ,getKeys))
+      }
+
+      return result
+}
+
+const result = student.getKeys()
+console.log(result)
